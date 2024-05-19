8a4c517a7a2687dde8fb80ecc85fcae4238edcef
Juan Sebastián <sebasurb01@icloud.com>
2024-05-19 18:03:26 -0500

    main
    actualizacion a solo una pagina

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import { auth } from '@/firebase/init'; // Importa tu inicialización de Firebase
 
-createApp(App).use(router).mount('#app')
+let app = null;
+
+// Asegúrate de que la instancia de Firebase Auth está lista antes de montar la aplicación
+auth.onAuthStateChanged(() => {
+  if (!app) {
+    app = createApp(App);
+    app.use(router);
+    app.mount('#app');
+  }
+}); 
