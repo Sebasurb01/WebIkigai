5b05e1ec26ea34b5e2393d576b3ad1d61340c8d0
Juan Sebastián <sebasurb01@icloud.com>
2024-05-19 13:05:58 -0500

    VISUAL
    LOGO Y LETRA

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,35 @@
 <template>
- <div>home</div>
- </template>
+  <div class="home-container">
+    <h1>BIENVENIDOS A</h1>
+
+    <h3 class="title is-3">
+         IKIGAI WEB
+        </h3>
+    <img :src="homeImage" alt="Home" class="home-image">
+  </div>
+</template>
 
 <script>
-// @ is an alias to /src
+import homeImage from '@/assets/logo1.png';  // Importa la imagen
 
 export default {
   name: 'HomeView',
-  components: {
-   
+  data() {
+    return {
+      homeImage  // Añade la imagen importada a los datos del componente
+    };
   }
 }
 </script>
+
+<style scoped>
+.home-container {
+  text-align: center;
+}
+
+.home-image {
+  max-width: 100%;
+  height: auto;
+  margin-top: 20px;
+}
+</style>
