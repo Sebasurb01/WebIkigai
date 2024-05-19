91d71e880fad020bb738f6311263d10bfa32d655
Juan Sebastián <sebasurb01@icloud.com>
2024-05-19 13:49:55 -0500

    visual
    se revirtio visual por error

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,38 @@
 <template>
- <div>home</div>
- </template>
+  <div class="home-container">
+    <h1>BIENVENIDOS A</h1> <h3 class="title is-3"> IKIGAI WEB </h3>
+    <img :src="homeImage" alt="Home" class="home-image">
+  </div>
+</template>
 
 <script>
-// @ is an alias to /src
+import homeImage from '@/assets/logo1.png';
 
 export default {
   name: 'HomeView',
-  components: {
-   
+  data() {
+    return {
+      homeImage
+    };
   }
 }
 </script>
+
+<style scoped>
+.home-container {
+  text-align: center;
+}
+
+.title {
+  font-family: 'Poppins', sans-serif;
+  font-weight: 600;
+  font-size: 2em;
+  margin-top: 20px;
+}
+
+.home-image {
+  max-width: 100%;
+  height: auto;
+  margin-top: 20px;
+}
+</style>
