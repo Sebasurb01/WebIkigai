6def44756350369e04b9a374f1203e6144549943
Juan Sebastián <sebasurb01@icloud.com>
2024-05-19 13:22:30 -0500

    Revert "VISUAL"
    This reverts commit 5b05e1ec26ea34b5e2393d576b3ad1d61340c8d0.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,14 @@
 <template>
-  <div class="home-container">
-    <h1>BIENVENIDOS A</h1>
-
-    <h3 class="title is-3">
-         IKIGAI WEB
-        </h3>
-    <img :src="homeImage" alt="Home" class="home-image">
-  </div>
-</template>
+ <div>home</div>
+ </template>
 
 <script>
-import homeImage from '@/assets/logo1.png';  // Importa la imagen
+// @ is an alias to /src
 
 export default {
   name: 'HomeView',
-  data() {
-    return {
-      homeImage  // Añade la imagen importada a los datos del componente
-    };
+  components: {
+   
   }
 }
 </script>
-
-<style scoped>
-.home-container {
-  text-align: center;
-}
-
-.home-image {
-  max-width: 100%;
-  height: auto;
-  margin-top: 20px;
-}
-</style>
