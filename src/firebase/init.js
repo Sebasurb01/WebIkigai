59e8674add0ae6cfbde3d09244efc98c51a16e32
Juan Sebastián <sebasurb01@icloud.com>
2024-05-19 12:46:55 -0500

    conexion firebase
    se tuvo problemas con la exportacion y lectura de auth ,se agergo getAuth modificaciones en registermenu y init

---
+++ src/firebase/init.js
@@ -0,0 +1,22 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth'; 
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDUO55aj19GL306A1WyLPrmq2KratLRLqw",
+  authDomain: "myprojectikigai.firebaseapp.com",
+  projectId: "myprojectikigai",
+  storageBucket: "myprojectikigai.appspot.com",
+  messagingSenderId: "466106609327",
+  appId: "1:466106609327:web:c761c13652d3c1eef12e5a",
+  measurementId: "G-JF72FPXWSR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Get a Firestore instance
+export const db = getFirestore(app);
+// Get an Auth instance
+export const auth = getAuth(app);
--- src/views/Auth/RegisterMenu.vue
+++ src/views/Auth/RegisterMenu.vue
@@ -1,71 +1,70 @@
 <template>
-
-<div class="container mt-50">
-<div class ="columns">
-<div class="column is-6 is-offset-3">
-    <h3 class="title is-3">Crear una cuenta</h3><hr>
-<form action="#" @submit.prevent="register">
-
-    <div class="field">
-  <label class="label">Name</label>
-  <div class="control">
-    <input class="input" type="text" placeholder="e.g Alex Smith" v-model="name">
+  <div class="container mt-50">
+    <div class="columns">
+      <div class="column is-6 is-offset-3">
+        <h3 class="title is-3">Crear una cuenta</h3>
+        <hr>
+        <form @submit.prevent="register">
+          <div class="field">
+            <label class="label">Name</label>
+            <div class="control">
+              <input class="input" type="text" placeholder="sebastian urbano" v-model="name">
+            </div>
+          </div>
+          <div class="field">
+            <label class="label">Email</label>
+            <div class="control">
+              <input class="input" type="email" placeholder="sebasurb@gmail.com" v-model="email">
+            </div>
+          </div>
+          <div class="field">
+            <label class="label">Contraseña</label>
+            <div class="control">
+              <input class="input" type="password" placeholder="******" v-model="password">
+            </div>
+          </div>
+          <button type="submit" class="button is-primary">Registrarme</button>
+        </form>
+        <div class="notification is-danger mt-10" v-if="error">
+          {{ error }}
+        </div>
+      </div>
+    </div>
   </div>
-</div>
-
-<div class="field">
-  <label class="label">Email</label>
-  <div class="control">
-    <input class="input" type="email" placeholder="e.g. alexsmith@gmail.com" v-model="email">
-  </div>
-</div>
-
-<div class="field">
-  <label class="label">Contrasena</label>
-  <div class="control">
-    <input class="input" type="password" placeholder="123asd" v-model="password">
-  </div>
-</div>
-
-<button type="submit" class="button is-primary">Registrarme</button>
-
-</form>
-<div class="notification is-danger mt-10 " v-if="error">
- {{ error }}
-</div>
-
-</div>
-</div>
-   </div>
 </template>
 
-
 <script>
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
-export default{
-data(){
+export default {
+  data() {
     return {
-name:'',
-email :'',
-password :'',
-error :''
-       }
+      name: '',
+      email: '',
+      password: '',
+      error: ''
+    };
   },
-    name:'RegisterMenu',
-    methods: {
-        register() {
-            if (this.name && this.email && this.password ){
-                //enviamos formularios
-        }else{ this.error = 'todos los campos son requeridos '
-
-                 }
-        } 
-        
-     }
-
-
-
-}
-    
-
+  name: 'RegisterMenu',
+  methods: {
+    register() {
+      this.error = '';
+      const auth = getAuth();
+      if (this.name && this.email && this.password) {
+        createUserWithEmailAndPassword(auth, this.email, this.password)
+          .then(userCredential => {
+            console.log(userCredential.user);
+            this.name = '';
+            this.email = '';
+            this.password = '';
+          })
+          .catch(err => {
+            this.error = err.message;
+          });
+      } else {
+        this.error = 'Todos los campos son requeridos';
+      }
+    }
+  }
+};
 </script>
