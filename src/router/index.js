9c376a330ca84a12c9c7f1946b4ebcf95696e377
Juan Sebastián <sebasurb01@icloud.com>
2024-05-19 17:20:13 -0500

    dashboard
    router modificadas seguridad inicio de seccion

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import HomeView from '../views/HomeView.vue'
 import LoginMenu from '../views/Auth/LoginMenu.vue'
 import RegisterMenu from '../views/Auth/RegisterMenu.vue'
 import DashboardView from '@/views/DashboardView.vue'
+import { auth } from '@/firebase/init' 
+
 const routes = [
   {
     path: '/',
@@ -10,28 +12,43 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/Login',
-    name: 'Login',
+    path: '/login',
+    name: 'login',
     component: LoginMenu
   },
- 
   {
-    path: '/Register',
-    name: 'Register',
+    path: '/register',
+    name: 'register',
     component: RegisterMenu
   },
-  
   {
-    path: '/Dashboard',
-    name: 'Dashboard',
-    component: DashboardView
+    path: '/dashboard',
+    name: 'dashboarduser',
+    component: DashboardView,
+    meta: {
+      requiresAuth: true
+    }
   }
- 
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(route => route.meta.requiresAuth)) {
+    const user = auth.currentUser;
+    if (user) {
+      next();
+    } else {
+      next({
+        name: 'login'
+      });
+    }
+  } else {
+    next();
+  }
 })
 
 export default router
