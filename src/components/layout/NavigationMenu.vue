1d7b393012bc84229232d0197c981a2e8a6dd955
Juan Sebastián <sebasurb01@icloud.com>
2024-05-19 19:25:52 -0500

    barra usuario
    se agrego barra para usuario con funcionalidad de cerrar seccion

--- src/components/layout/NavigationMenu.vue
+++ src/components/layout/NavigationMenu.vue
@@ -1,65 +1,100 @@
 <template>
-   
-   <nav class="navbar" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-    <router-link  class="navbar-item" to="/">
-
-      <h3 class="title is-3">
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <router-link class="navbar-item" to="/">
+        <h3 class="title is-3">
           <img :src="logo" alt="Icono" class="logo-image"> IKIGAI
         </h3>
-    </router-link>
-
-    <a role="button" class="navbar-burger" :class="{'is-active': isOpen }" @click.prevent="toggleMenu"  aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
-  </div>
-
-  <div id="navbarBasicExample" class="navbar-menu" :class="{'is-active': isOpen }" >
-    <div class="navbar-start">
-      <router-link class="navbar-item" to="/">
-        Home
       </router-link>
 
+      <a role="button" class="navbar-burger" :class="{'is-active': isOpen}" @click.prevent="toggleMenu" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
     </div>
 
-    <div class="navbar-end">
-      <div class="navbar-item">
-        <div class="buttons">
-          <router-link  class="button is-primary" to="/Register">
-            <strong>Registarme</strong>
-          </router-link>
-          <router-link class="button is-light" to ="/Login">
-            Iniciar Seccion
-          </router-link>
+    <div id="navbarBasicExample" class="navbar-menu" :class="{'is-active': isOpen}">
+      <div class="navbar-start">
+        <router-link class="navbar-item" to="/">
+          Home
+        </router-link>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+
+
+          <template v-if="user">
+            <div class="navbar-item has-dropdown is-hoverable">
+              <a class="navbar-link">
+                USUARIO
+              </a>
+              <div class="navbar-dropdown">
+                <a class="navbar-item">
+                  Informacion 
+                </a>
+                <a class="navbar-item" @click="logout">
+                  Cerrar Sesión
+                </a>
+                <a class="navbar-item">
+                  Contacto
+                </a>
+               
+                <a class="navbar-item">
+                  Reportar Error
+                </a>
+              </div>
+            </div>
+          </template>
+
+          <template v-else>
+            <div class="buttons">
+              <router-link class="button is-primary" to="/register">
+                <strong>Registarme</strong>
+              </router-link>
+              <router-link class="button is-light" to="/login">
+                Iniciar Sesión
+              </router-link>
+            </div>
+          </template>
         </div>
       </div>
     </div>
-  </div>
-</nav>
-  </template>
-  
-  <script>
-  export default {
-    data() {
+  </nav>
+</template>
+
+<script>
+import { onAuthStateChanged, getAuth, signOut } from "firebase/auth";
+//import { ref, onMounted } from "vue";
+
+export default {
+  data() {
     return {
-      logo: require('@/assets/logo.png'
-     
-      ),
       isOpen: false,
+      logo: require('@/assets/logo.png'),
+      user: null,
     };
   },
-
-methods: {
-  toggleMenu(){  
-    const status=!this.isOpen
-    this.isOpen=status
-   }
-}
-
- 
+  methods: {
+    toggleMenu() {
+      this.isOpen = !this.isOpen;
+    },
+    logout() {
+      const auth = getAuth();
+      signOut(auth).then(() => {
+        this.$router.push({ name: 'login' });
+      }).catch((error) => {
+        console.error('Error signing out:', error);
+      });
+    },
+  },
+  mounted() {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      this.user = user ? user : null;
+    });
+  },
 };
 </script>
 
